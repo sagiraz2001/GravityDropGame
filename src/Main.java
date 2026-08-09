3e4bf1090dfea8feee6f4e25267829018ae8cfca
Sagi Raz <razzsagi2001@gmail.com>
2026-08-09 16:23:02 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Gravity Drop Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setSize(800, 600);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+}
